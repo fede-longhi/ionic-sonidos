18da0d7684e3e80f91d7e759095f9e781a1e9ab8
fede-longhi <federico.rlonghi@gmail.com>
2018-03-25 13:29:45 -0300

    funcionalidad reproducir y pausar sonido

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,14 +11,45 @@ import { Animal } from "../../interfaces/animal.interface"
 export class HomePage {
 
   animales:Animal[] = [];
+  audio = new Audio();
+  audioTiempo:any;
 
   constructor(public navCtrl: NavController) {
     this.animales = ANIMALES.splice(0);
-
   }
 
-  reproducir (animal:Animal){
+  private pausar_audio( animalSel: Animal){
+    clearTimeout( this.audioTiempo );
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for (let animal of this.animales){
+      if (animal.nombre != animalSel.nombre){
+        animal.reproduciendo = false;
+      }
+    }
+  }
+
+  reproducir( animal:Animal ){
+    this.pausar_audio(animal);
+
+    if (animal.reproduciendo){
+      animal.reproduciendo = false;
+      return;
+    }
+
     console.log(animal);
+
+    this.audio.src = animal.audio;
+
+    this.audio.load();
+    this.audio.play();
+
+    animal.reproduciendo = true;
+
+    this.audioTiempo = setTimeout( ()=>animal.reproduciendo = false, animal.duracion * 1000  );
+
+
   }
 
 }
